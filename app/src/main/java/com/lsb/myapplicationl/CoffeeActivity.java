e47a599025666d8a0f5085cb236c8cd314d9bdf8
Sangbae <allfogg@naver.com>
2017-07-20 14:18:28 +0900

    Coffee 주문앱 주문자, 토핑, 가격 및 표시 여부 추가 및 수정

--- app/src/main/java/com/lsb/myapplicationl/CoffeeActivity.java
+++ app/src/main/java/com/lsb/myapplicationl/CoffeeActivity.java
@@ -3,13 +3,23 @@ package com.lsb.myapplicationl;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+
 public class CoffeeActivity extends AppCompatActivity {
+    public static final int DEFAULT_QUANTITY = 1;
+    public static final int PRICE = 3000;
+
+    private int mQuantity = DEFAULT_QUANTITY;
+
     private TextView mQuantityTextView;
-    private int mQuantity = 1;
     private TextView mPriceTextView;
-    private int mPrice =0;
+    private CheckBox mWippedCreamCheckBox;
+    private EditText mNameEditText;
+    private DecimalFormat mFormat = new DecimalFormat("#,##0");
 
 
     // AppCompatActivity 구글이 제공하는 액티비티가 들어 있는 곳
@@ -26,31 +36,42 @@ public class CoffeeActivity extends AppCompatActivity {
         mQuantityTextView = (TextView) findViewById(R.id.volume);
         mPriceTextView = (TextView) findViewById(R.id.price);
 
-        mQuantityTextView.setText(String.valueOf(mQuantity));
+        mQuantityTextView.setText(String.valueOf(DEFAULT_QUANTITY));
 //        mQuantityTextView.setText("" + mQuantity);  이 방법을 많이 쓰긴함 간단하여
+        mWippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_check);
+        mNameEditText = (EditText) findViewById(R.id.name_edit);
+
+        display();
     }
 
     public void minusButtonClicked(View view) {
 //        Toast.makeText(this, "잘 눌림", Toast.LENGTH_SHORT).show();
-        if (mQuantity > 1) {
-            mQuantity--;
-            mPrice = mPrice - 3000;
-        } else {
-            mQuantity = 1;
-            mPrice = 3000;
+        mQuantity--;
+        if (mQuantity < 1) {
+            mQuantity = DEFAULT_QUANTITY;
         }
-        mQuantityTextView.setText("" + mQuantity );
-        mPriceTextView.setText("" + mPrice+ "원");
+        display();
     }
-
 
 
     public void plusBottonCliced(View view) {
         mQuantity++;
-        mPrice = mPrice + 3000;
-        mPriceTextView.setText("" + mPrice+ "원");
-        mQuantityTextView.setText("" + mQuantity );
+        display();
     }
 
 
+    public void onCheckBoxClicked(View view) {
+        display();
+    }
+
+    private void display() {
+        mQuantityTextView.setText("" + mQuantity);
+        String message = "주문자 : " + mNameEditText.getText().toString();
+        message += "\n====================";
+        message += "\n휘핑 크림 추가 여부 : " + mWippedCreamCheckBox.isChecked();
+        message += "\n갯수 : " + mQuantity;
+        message += "\n가격 : " + mFormat.format(mQuantity * PRICE) + "원";
+
+        mPriceTextView.setText(message);
+    }
 }
